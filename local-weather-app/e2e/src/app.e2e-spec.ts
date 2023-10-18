bd679ca52396a47974359b74ad489b86146d7926
duda <duda_ebbers@hotmail.com>
2023-10-18 15:00:47 -0300

    e2e e configs

---
+++ local-weather-app/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,28 @@
+import { browser, logging } from "protractor";
+import { AppPage } from "./app.po"
+import { async } from '@angular/core/testing';
+
+describe('workspace-project App', () => {
+    let page: AppPage;
+
+    beforeEach(() => {
+        page = new AppPage();
+    });
+
+    it('should display welcome message', () => {
+        page.navigateTo();
+        expect(page.getTitleText()).toEqual('LocalCast Weather');
+    });
+
+    afterEach(async() => {
+        //Assert that there are no errors emitted from browser
+        const logs = await browser
+            .manage()
+            .logs()
+            .get(logging.Type.BROWSER);
+        
+        expect(logs).not.toContain(jasmine.objectContaining({
+            level: logging.Level.SEVERE
+        } as logging.Entry))
+    })
+})

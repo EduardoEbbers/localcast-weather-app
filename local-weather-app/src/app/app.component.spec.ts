c873e689383f6b3a771e71b1b14a98bdffe7fb5d
duda <duda_ebbers@hotmail.com>
2023-10-18 01:22:29 -0300

    correcao dos arquivos .spec

--- local-weather-app/src/app/app.component.spec.ts
+++ local-weather-app/src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+    imports: [RouterTestingModule, HttpClientTestingModule],
+    declarations: [AppComponent, CurrentWeatherComponent]
+  }).compileComponents());
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+  /*
   it(`should have as title 'local-weather-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('local-weather-app');
   });
+  */
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('local-weather-app app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('LocalCast Weather');
   });
 });
--- local-weather-app/src/app/current-weather/current-weather.component.spec.ts
+++ local-weather-app/src/app/current-weather/current-weather.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CurrentWeatherComponent } from './current-weather.component';
+import { WeatherService } from '../weather/weather.service';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
@@ -8,8 +9,11 @@ describe('CurrentWeatherComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CurrentWeatherComponent]
+      declarations: [CurrentWeatherComponent],
+      providers: [WeatherService],
+      imports: [HttpClientTestingModule]
     });
+    
     fixture = TestBed.createComponent(CurrentWeatherComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();

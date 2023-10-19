31ad1f75abaf0ec22038f3a898f2c0dc6a7ea851
duda <duda_ebbers@hotmail.com>
2023-10-19 01:41:07 -0300

    configuration material and module material; config. angular flex layout

--- local-weather-app/src/app/app.module.ts
+++ local-weather-app/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { CurrentWeatherComponent } from './current-weather/current-weather.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
